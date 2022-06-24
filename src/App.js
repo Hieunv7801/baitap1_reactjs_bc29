da10b3e32f403f8b2c9717dcf8c7934c8a7363c3
Hieunv7801 <hieunv0708@gmail.com>
2022-06-24 23:43:54 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import BaitapComponent from './component/BaiTap/BaitapComponent';
+
+function App() {
+  return (
+    <div>
+      <BaitapComponent/>
+    </div>
+  );
+}
+
+export default App;
